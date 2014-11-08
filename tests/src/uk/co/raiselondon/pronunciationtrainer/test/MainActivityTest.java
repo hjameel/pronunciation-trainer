ec15a8e1ff604ce75a2c762b373fc294835fe95d
Hanif Jameel <hanif.jameel@btinternet.com>
2014-11-08 20:33:59 +0000

    Reformat source files.

--- tests/src/uk/co/raiselondon/pronunciationtrainer/test/MainActivityTest.java
+++ tests/src/uk/co/raiselondon/pronunciationtrainer/test/MainActivityTest.java
@@ -6,27 +6,27 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-	
-	private MainActivity mainActivity;
-	private TextView textView;
 
-	public MainActivityTest() {
-		super(MainActivity.class);
-	}
-	
+    private MainActivity mainActivity;
+    private TextView textView;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mainActivity = getActivity();
         textView = (TextView) mainActivity.findViewById(R.id.myText);
     }
-    
+
     public void testPreconditions() {
         assertNotNull("mainActivity is null", mainActivity);
         assertNotNull("textView is null", textView);
     }
-    
+
     public void testThatHelloWorldIsDisplayed() {
-    	assertEquals("Hello world!", textView.getText().toString());
+        assertEquals("Hello world!", textView.getText().toString());
     }
 }
