9356ada337a630d0a75dd36fef2be4ff9759e705
Hanif Jameel <hanif.jameel@btinternet.com>
2014-11-09 19:18:21 +0000

    read and play an mp3

--- src/uk/co/raiselondon/pronunciationtrainer/MainActivity.java
+++ src/uk/co/raiselondon/pronunciationtrainer/MainActivity.java
@@ -1,7 +1,21 @@
 package uk.co.raiselondon.pronunciationtrainer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,7 +24,65 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        File file = getAlbumStorageDir(this, "thingyy");
+        Writer writer = null;
+        try {
+            FileOutputStream fo = new FileOutputStream(file);
+            writer = new BufferedWriter(new OutputStreamWriter(fo));
+            writer.write("Something");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        file = getAlbumStorageDir(this, "ahanava.mp3");
+            FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            MediaPlayer mp = new MediaPlayer();
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mp.setDataSource(fis.getFD());
+            mp.prepare();
+            mp.start();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
         setContentView(R.layout.activity_main);
+    }
+
+    public File getAlbumStorageDir(Context context, String fileName) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(null), fileName);
+        return file;
     }
 
     @Override
